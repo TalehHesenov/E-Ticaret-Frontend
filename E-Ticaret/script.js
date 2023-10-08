566f55df14e41fb843c0b57de85c75dd25b39e60
TalehHesenov <127445846+TalehHesenov@users.noreply.github.com>
2023-10-08 14:12:27 +0300

    Blog ve Shop seting
    Yeni blog.html shop.html ve sproduct.html elave olundu duzelisler edildi

--- E-Ticaret/script.js
+++ E-Ticaret/script.js
@@ -2,6 +2,21 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+}
 
 if(bar){
     bar.addEventListener('click', () => {
